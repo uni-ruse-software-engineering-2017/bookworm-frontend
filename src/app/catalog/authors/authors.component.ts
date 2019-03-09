02f5d4320ec2af441cd1707dc8a63b091defb600
Tsvetan Ganev <ts.ganev@apihawk.com>
2019-03-09 15:32:25 +0200

    [#1] (Customer) Adds pagination for authors

--- src/app/catalog/authors/authors.component.ts
+++ src/app/catalog/authors/authors.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { PageEvent } from "@angular/material";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  map,
+  startWith
+} from "rxjs/operators";
 import { AuthorService } from "src/app/core/services/author.service";
+import { IPaginatedResource } from "src/app/core/types";
 import { IAuthorListItem } from "src/app/core/types/catalog";
+import { emptyResource, IPaginationQuery } from "src/app/util/pagination";
 
 @Component({
   selector: "bw-authors",
@@ -8,13 +19,53 @@ import { IAuthorListItem } from "src/app/core/types/catalog";
   styleUrls: ["./authors.component.scss"]
 })
 export class AuthorsComponent implements OnInit {
-  authors: IAuthorListItem[];
+  authors: IPaginatedResource<IAuthorListItem> = emptyResource();
+  searchToggled = false;
+  searchInput = new FormControl();
 
   constructor(public authorService: AuthorService) {}
 
   ngOnInit() {
-    this.authorService.getAll().subscribe(authors => {
-      this.authors = authors.items;
+    this.getAuthors();
+
+    this.searchInput.valueChanges
+      .pipe(
+        startWith(""),
+        debounceTime(600),
+        filter((sn: string) => sn && sn.length >= 2),
+        distinctUntilChanged(),
+        map((searchString: string) =>
+          this.getAuthors({ page: 1, pageSize: 10, search: searchString })
+        )
+      )
+      .subscribe();
+
+    // if an user deletes the search string,
+    // load the initial list of authors
+    this.searchInput.valueChanges.subscribe((val: string) => {
+      if (val !== "") {
+        return;
+      }
+
+      this.getAuthors();
     });
+  }
+
+  getAuthors(query?: IPaginationQuery) {
+    this.authorService.getAll(query).subscribe(authors => {
+      this.authors = authors;
+    });
+  }
+
+  onPaginate(event: PageEvent) {
+    this.getAuthors({ page: event.pageIndex + 1, pageSize: event.pageSize });
+  }
+
+  toggleSearch(state: boolean) {
+    this.searchToggled = state;
+
+    if (!state) {
+      this.searchInput.setValue("");
+    }
   }
 }
